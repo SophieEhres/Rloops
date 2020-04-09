7b4a8e73f746e76d478276b8ea26c63e9773e39e
SophieEhres <sophie.ehresmann@gmail.com>
2020-04-09 09:50:33 -0400

    added an "all" gtf file

--- consensus_peaks.r
+++ consensus_peaks.r
@@ -1,27 +1,29 @@
 #!/bin/R
 #Merge Consensus peaks (RNH+NT)
+wd<-getwd()
+
 setwd("/Users/ehresms/computational/rloop/merged_bed")
+con_dir<-"/Users/ehresms/computational/rloop/consensus_bed"
+dir.create(con_dir)
 
 library(rtracklayer)
 library(GenomicRanges)
 
 files<-list.files(".")
-names<-sapply(strsplit(files, split = "_"), `[`, 1)
+names<-unique(sapply(strsplit(files, split = "-"), `[`, 1))
 
-rnase_files<-unique(c(grep(names, pattern = "RNH", value = TRUE), grep(names, pattern = "RNase", value = TRUE)))
-
-for (name in unique(names)) {
+for (name in names) {
   files<-grep(list.files("."), pattern = name, value = TRUE)
   
-  peak_files <-grep(files, pattern = "forward", value = TRUE)
-  peak_granges <- lapply(peak_files, import)
-  peak_grangeslist <- GRangesList(peak_granges)
+  peak_files<-grep(files, pattern = "forward", value = TRUE)
+  peak_granges<-lapply(peak_files, import)
+  peak_grangeslist<-GRangesList(peak_granges)
   peak_coverage <- coverage(peak_grangeslist)
   covered_ranges <- slice(peak_coverage, lower=2, rangesOnly=T)
   covered_granges <- GRanges(covered_ranges)
   #covered_final<-reduce(covered_granges, min.gapwidth=31)
   
-  export(covered_granges, paste0("~/computational/rloop/merged_bed/consensus_peaks_", name, "_forward.bed"))
+  export(covered_granges, paste0(con_dir,"/consensuspeaks_", name, "_forward.bed"))
   
   peak_files<-grep(files, pattern = "reverse", value = TRUE)
   peak_granges<-lapply(peak_files, import)
@@ -31,5 +33,30 @@ for (name in unique(names)) {
   covered_granges <- GRanges(covered_ranges)
   #covered_final<-reduce(covered_granges, min.gapwidth=31)
   
-  export(covered_granges, paste0("~/computational/rloop/merged_bed/consensus_peaks_", name, "_reverse.bed")) }
+  export(covered_granges, paste0(con_dir,"/consensuspeaks_", name, "_reverse.bed")) }
 
+
+
+files<-list.files(".")
+
+peak_files<-grep(files, pattern = "forward", value = TRUE)
+peak_granges<-lapply(peak_files, import)
+peak_grangeslist<-GRangesList(peak_granges)
+peak_coverage <- coverage(peak_grangeslist)
+covered_ranges <- slice(peak_coverage, lower=2, rangesOnly=T)
+covered_granges <- GRanges(covered_ranges)
+#covered_final<-reduce(covered_granges, min.gapwidth=31)
+
+export(covered_granges, paste0(con_dir,"/consensuspeaks_all", "_forward.bed"))
+
+peak_files<-grep(files, pattern = "reverse", value = TRUE)
+peak_granges<-lapply(peak_files, import)
+peak_grangeslist<-GRangesList(peak_granges)
+peak_coverage <- coverage(peak_grangeslist)
+covered_ranges <- slice(peak_coverage, lower=2, rangesOnly=T)
+covered_granges <- GRanges(covered_ranges)
+#covered_final<-reduce(covered_granges, min.gapwidth=31)
+
+export(covered_granges, paste0(con_dir,"/consensuspeaks_", "all_reverse.bed")) 
+
+setwd(wd)

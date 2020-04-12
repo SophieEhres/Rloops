58b7ee1a2e2b85bad8f35e58ea855b47d0890fb6
SophieEhres <sophie.ehresmann@gmail.com>
2020-04-12 18:42:01 -0400

    modified for TSS/center

--- computematrix.sh
+++ computematrix.sh
@@ -1,48 +1,35 @@
 #!/bin/bash
 
-dir0=~/computational
-bwdir=$dir0/rloop/bigwig
-genome=$dir0/genomes/droso/annotations/final_dmel-all-r6.32.bed
-matdir=$dir0/rloop/matrix
+dir0="/Users/ehresms/computational"
+bwdir=${dir0}/rloop/bigwig
+genome=${dir0}/genomes/droso/annotations/final_dmel-all-r6.32.bed
+matdir=${dir0}/rloop/matrix
 
-mkdir -p $matdir
+mkdir -p ${matdir}
 
-conditions="S2 6 10"
-orientations="forward reverse"
-treatments="IP input RN_IP RN_input"
+conditions=$(ls ${bwdir} | grep -e "final" | cut -d "-" -f1 | uniq)
 
-cd $bwdir
 
-for condition in $conditions; do
+for condition in ${conditions}; do
+	echo "computing for ${condition}"
+
+	files=$(ls $bwdir/*.bw | grep -e $condition | grep -e "final" | tr "\n" " " )
 	
-	for orientation in $orientations; do	
+	echo "files are ${files}"
 
-		files=$(ls $bwdir | grep -e $orientation)
+	computeMatrix reference-point --referencePoint TSS \
+		-b 2500 -a 2500 \
+		-R ${genome} \
+		-S ${files} \
+		-p 12 \
+		-o ${matdir}/${condition}_TSS.gz \
+	
+	computeMatrix scale-regions\
+		-b 1500 -a 1500 \
+		-R ${genome} \
+		-S ${files} \
+		-p 12 \
+		-o ${matdir}/${condition}_mid.gz \
 
-		IP=$(echo "$files" | grep -e $condition | grep -v "RN" | grep -v "input" | tr '\n' ' ')
-		input=$(echo "$files" | grep -e $condition | grep -v "RN" | grep -e "input" | tr '\n' ' ')
-		RN_IP=$(echo "$files" | grep -e $condition | grep -e "RN" | grep -v "input" | tr '\n' ' ')
-		RN_input=$(echo "$files" | grep -e $condition | grep -e "RN" | grep -e "input" | tr '\n' ' ')
 
-
-		for treatment in $treatments; do
-
-			computeMatrix reference-point --referencePoint TSS \
-				-b 1000 -a 3000 \
-				-R $genome \
-				-S $(echo "${!treatment}") \
-				-p 12 \
-				-o $matdir/${condition}_${treatment}_${orientation}_TSS.gz \
-			
-			computeMatrix\
-				-b 1000 -a 3000 \
-				-R $genome \
-				-S $(echo "${!treatment}") \
-				-p 12 \
-				-o $matdir/${condition}_${treatment}_${orientation}_mid.gz \
-
-		done
-	done
 done
-
-cd $dir0/scripts

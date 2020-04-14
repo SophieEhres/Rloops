517ccf8580fc1c092bbdebd097f4df779873e3c0
SophieEhres <sophie.ehresmann@gmail.com>
2020-04-14 12:24:06 -0400

    modified for real TSS/midpoint... scale regions and reference for all

--- computematrix.sh
+++ computematrix.sh
@@ -4,18 +4,36 @@ dir0="/Users/ehresms/computational"
 bwdir=${dir0}/rloop/bigwig
 genome=${dir0}/genomes/droso/annotations/final_dmel-all-r6.32.bed
 matdir=${dir0}/rloop/matrix
+beddir=${dir0}/rloop/consensus_bed
+all_reference=$(ls ${dir0}/rloop/consensus_bed/*.bed | grep -e "all")
 
 mkdir -p ${matdir}
 
 conditions=$(ls ${bwdir} | grep -e "final" | cut -d "-" -f1 | uniq)
+
+if echo "checking ${all_reference} file" | grep -v "both"; then
+	cat ${all_reference} > ${beddir}/consensus_peak_all_both.bed
+fi
+
+all_reference_file=${beddir}/consensus_peak_all_both.bed
 
 
 for condition in ${conditions}; do
 	echo "computing for ${condition}"
 
 	files=$(ls $bwdir/*.bw | grep -e $condition | grep -e "final" | tr "\n" " " )
-	
+
 	echo "files are ${files}"
+
+	reference=$(ls ${beddir}/*.bed | grep -e ${condition})
+
+		
+	if echo "checking ${reference} file" | grep -v "both"; then
+		cat ${reference} > ${beddir}/consensus_peak_${condition}_both.bed
+	fi
+
+	reference_file=${beddir}/consensus_peak_${condition}_both.bed
+
 
 	computeMatrix reference-point --referencePoint TSS \
 		-b 2500 -a 2500 \
@@ -29,7 +47,34 @@ for condition in ${conditions}; do
 		-R ${genome} \
 		-S ${files} \
 		-p 12 \
-		-o ${matdir}/${condition}_mid.gz \
+		-o ${matdir}/${condition}_TSS-scale.gz \
 
+	computeMatrix reference-point --referencePoint TSS \
+		-b 2500 -a 2500 \
+		-R ${reference_file} \
+		-S ${files} \
+		-p 12 \
+		-o ${matdir}/${condition}_reference.gz \
+
+	computeMatrix scale-regions \
+		-b 2500 -a 2500 \
+		-R ${reference_file} \
+		-S ${files} \
+		-p 12 \
+		-o ${matdir}/${condition}_reference-scale.gz \
+
+	computeMatrix reference-point --referencePoint TSS \
+		-b 2500 -a 2500 \
+		-R ${all_reference_file} \
+		-S ${files} \
+		-p 12 \
+		-o ${matdir}/${condition}_reference-all.gz \
+
+	computeMatrix scale-regions \
+		-b 2500 -a 2500 \
+		-R ${all_reference_file} \
+		-S ${files} \
+		-p 12 \
+		-o ${matdir}/${condition}_reference-all-scale.gz \
 
 done

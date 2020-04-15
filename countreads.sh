7a77f296493da8c138b2f2d6bde8f6d981baa74a
SophieEhres <sophie.ehresmann@gmail.com>
2020-04-15 16:30:11 -0400

    modified to work with new annotation gtf

--- countreads.sh
+++ countreads.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-dir0=${computational}
+dir0=/Users/ehresms/computational
 bamdir=${dir0}/rloop/align
 countdir=${dir0}/rloop/counts
-gtfdir=${dir0}/rloop/gtf
+gtfdir=${dir0}/rloop/consensus_gtf
 
 mkdir -p ${countdir}
 
-for file in $(ls ${bamdir}); do
+for file in $(ls ${bamdir}/*.bam); do
 
     echo "counting ${file}"
 
-    name=$(echo ${file} | cut -d "_" -f1)
-    anno=$(ls ${gtfdir} | grep -e "${name}")
+    name=$(echo ${file%????} | rev | cut -d "/" -f1 | rev)
+    
+    if echo "checking ${name} annotation" | grep -e "6" ; then
+        anno=$(ls ${gtfdir}/*.gtf | grep -e "annotation" | grep -e "both" | grep -e "2_")
 
-    countFeatures -T 16 -a ${gtfdir}/${anno} \
-        -p -F -G -M --fraction \
+    elif echo "checking ${name} annotation" | grep -e "10"; then
+        anno=$(ls ${gtfdir}/*.gtf | grep -e "annotation" | grep -e "both" | grep -e "10")
+
+    elif echo "checking ${name} annotation" | grep -e "cells"; then
+        anno=$(ls ${gtfdir}/*.gtf | grep -e "annotation" | grep -e "both" | grep -e "cells")
+
+    else
+        echo "problem with annotation"
+    fi    
+        echo "annotation is ${anno}"
+
+    featureCounts -T 16 -p \
+        -M --fraction \
+        -t "exon" \
+        -g "test_id" \
+        -s 1 \
+        -F GTF \
+        -a ${anno} \
         -o ${countdir}/${name} \
         ${file}
 

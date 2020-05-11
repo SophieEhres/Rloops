d884f9f7c6ca7268b0a524cc1e3edf76485dbe32
SophieEhres <sophie.ehresmann@gmail.com>
2020-05-11 12:24:16 -0400

    modified to unzip from gzipped files

--- bowtie_align.sh
+++ bowtie_align.sh
@@ -25,10 +25,37 @@ mkdir ${aligndir}
 
 			file1=$(ls ${fastadir}/*.fq |grep -e ${name}_ |grep -e "R1" )  
 			file2=$(ls ${fastadir}/*.fq |grep -e ${name}_ |grep -e "R2" )
+
+		
+			if [ -z ${file1} ]; then
+
+				echo "unzipping files"
+
+				zipped="yes"
+
+				zipped1=$(ls ${fastadir}/*.gz |grep -e ${name}_ |grep -e "R1" )  
+				tar -zxvf ${zipped1} -C ${fastadir}
+				
+				zipped2=$(ls ${fastadir}/*.gz |grep -e ${name}_ |grep -e "R2" )  
+				tar -zxvf ${zipped2} -C ${fastadir}
+
+
+				file1=$(ls ${fastadir}/*.fq |grep -e ${name}_ |grep -e "R1" )  
+				file2=$(ls ${fastadir}/*.fq |grep -e ${name}_ |grep -e "R2" )
+				
+			fi
+
+
 			echo "file1 is ${file1}, file 2 is ${file2}"  
 
 			 bowtie2 -q -p 12 -x ${genomedir}/bowtie_droso -1 ${file1} -2 ${file2} \
 				 -S ${aligndir}/${name}_aligned.sam
+			
+			if [ ${zipped} == "yes" ]; then
+				rm $file1 $file2
+
+			fi
+			
 		fi
 
 	done

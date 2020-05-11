e3455d2cfafe6df4f26a80580f87f0146fdf247d
SophieEhres <sophie.ehresmann@gmail.com>
2020-05-11 12:24:35 -0400

    modified to not take unmapped reads

--- split_sam.sh
+++ split_sam.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-dir0=~/computational/rloop
-aligndir=$dir0/align
-splitdir=$dir0/split_sam
+dir0="/Users/ehresms/computational/rloop"
+aligndir=${dir0}/align_trimmed
+splitdir=${dir0}/split_sam
 
-dirs=$(ls $aligndir)
+files=$(ls ${aligndir})
 
-mkdir $splitdir
+mkdir ${splitdir}
 
-for dir in $dirs; do
-	files=$(ls $aligndir/$dir)
-	targetdir=$splitdir/$dir
-	mkdir $targetdir
+for file in ${files}; do
 
-	for file in $files; do
-		name=$(echo $file | cut -d '.' -f1)
-		samtools view -h -f 18 $aligndir/$dir/$file > $targetdir/${name}_reverse.sam
-		samtools view -h -f 34 $aligndir/$dir/$file > $targetdir/${name}_forward.sam
-		
-	done
+	name=$(echo $file | cut -d '.' -f1)
+	
+	samtools view -h -f 18 -F 4 ${aligndir}/${file} > ${splitdir}/${name}_reverse.sam
+	samtools view -h -f 34 -F 4 ${aligndir}/${file} > ${splitdir}/${name}_forward.sam
+
+	check=$(samtools quickcheck ${splitdir}/${name}_forward.sam)
+
+	if [ -z "${check}" ]; then
+		echo "split successful, removing original file"
+
+		# rm ${aligndir}/${file}
+
+	else
+		echo "split not successful"
+
+	fi
+
+	
 done
+

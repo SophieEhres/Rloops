8c4ae5d3f2046614355ad3fa24cd1b1c33eba7b1
SophieEhres <sophie.ehresmann@gmail.com>
2020-05-13 11:27:46 -0400

    modified to delete original sam file and to verify integrity of sam

--- split_sam.sh
+++ split_sam.sh
@@ -10,23 +10,32 @@ mkdir ${splitdir}
 
 for file in ${files}; do
 
+	check="bad"
+
 	name=$(echo $file | cut -d '.' -f1)
+
+	if [ -f ${splitdir}/${name}_forward.sam ]; then
+		echo "already split"
 	
-	samtools view -h -f 18 -F 4 ${aligndir}/${file} > ${splitdir}/${name}_reverse.sam
-	samtools view -h -f 34 -F 4 ${aligndir}/${file} > ${splitdir}/${name}_forward.sam
-
-	check=$(samtools quickcheck ${splitdir}/${name}_forward.sam)
-
-	if [ -z "${check}" ]; then
-		echo "split successful, removing original file"
-
-		# rm ${aligndir}/${file}
-
 	else
-		echo "split not successful"
+
+		samtools view -h -f 18 -F 4 -@ 16 ${aligndir}/${file} > ${splitdir}/${name}_reverse.sam
+		samtools view -h -f 34 -F 4 -@ 16 ${aligndir}/${file} > ${splitdir}/${name}_forward.sam
+
+		check=$(samtools quickcheck ${splitdir}/${name}_forward.sam)
+
+		if [ -z "${check}" ]; then
+			echo "split successful, removing original file"
+
+			rm ${aligndir}/${file}
+
+		else
+
+			echo "split not successful"
+
+		fi
 
 	fi
-
 	
 done
 

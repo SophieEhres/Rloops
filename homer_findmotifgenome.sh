7a6a6a54ae42eb727c4165864416636b35c806a6
SophieEhres <sophie.ehresmann@gmail.com>
2020-05-21 17:43:53 -0400

    adding new functions NOT FINAL

--- homer_findmotifgenome.sh
+++ homer_findmotifgenome.sh
@@ -2,14 +2,18 @@
 
 analyzedir="/Users/ehresms/computational/rloop/analysis"
 genome="/Users/ehresms/computational/genomes/droso/fasta/dmel-all-chromosome-r6.32.fasta"
+gtf="/Users/ehresms/computational/genomes/droso/annotations/dmel-all-r6.32.gtf"
 
-for file in $(ls ${analyzedir}/*.bed); do
+
+for file in $(ls ${analyzedir}/*.bed | grep -e "sensitive"); do
 
     name=$(echo ${file} | xargs basename | cut -d "." -f1 )
     
     echo "finding motifs for ${name}"
 
-    findMotifsGenome.pl ${file} ${genome} ${analyze} ${analyzedir}/${name}_homer \
-        -size 200
-
+    findMotifsGenome.pl ${file} ${genome} ${analyze} ${analyzedir}/${name}_homertest \
+        -size 200 -p 16
+    
+    annotatePeaks.pl ${file} ${genome} \
+        -gtf ${gtf} -raw 
 done

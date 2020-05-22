b410dc388da82edc85fb5655b275a0f1c87a5cc7
SophieEhres <sophie.ehresmann@gmail.com>
2020-05-22 10:44:46 -0400

    modified to work without reps and added log file

--- peaktobed.sh
+++ peaktobed.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-dir0=~/computational
-peakdir=$dir0/rloop/first_peak
-beddir=$dir0/rloop/bed
+dir0="/Users/ehresms/computational"
+peakdir=${dir0}/rloop/peaks
+beddir=${dir0}/rloop/bed
 
-mkdir -p $beddir
+mkdir -p ${beddir}
 
-for dir in $(ls $peakdir); do
-	files=$(ls $peakdir/$dir | cut -d "_" -f1-3 | uniq)
-	
-	for file in $files; do
-		forwardfile=$(ls $peakdir/$dir | grep -e "$file" | grep -e "forward")
-		reversefile=$(ls $peakdir/$dir | grep -e "$file" | grep -e "reverse")
+for dir in $(ls ${peakdir}); do
 		
-		cat $peakdir/$dir/$forwardfile/${forwardfile}_peaks.xls | tail -n+30 | \
-		awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$10,$9}' >> $beddir/${file}_forward.bed
-		cat $peakdir/$dir/$reversefile/${reversefile}_peaks.xls | tail -n+30 | \
-		awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$10,$9}' >> $beddir/${file}_reverse.bed
-	done
+		cat $peakdir/$dir/*_peaks.xls | tail -n+30 | \
+		awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$10,$9}' >> $beddir/${dir}.bed
+	
 done
+
+

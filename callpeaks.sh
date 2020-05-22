6413550eb6a117ea73beb4acf102517959617ab1
SophieEhres <sophie.ehresmann@gmail.com>
2020-05-22 10:45:49 -0400

    added log file

--- callpeaks.sh
+++ callpeaks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exec 2>~/computational/scripts/callpeaks.log
+exec 2>./callpeaks.log
 
 dir0="/Users/ehresms/computational"
 splitdir="${dir0}/rloop/split_sam"
@@ -12,23 +12,41 @@ names=$(ls ${splitdir} | cut -d "_" -f1-2 | uniq | grep -v "input" )
 
 for name in ${names}; do
 
-		echo "calling peaks for ${name}"
+		echo "checking if ${name} peak files exist in peak directory"
 
-		controlname=$(echo ${name} | rev | cut -d "-" -f2- | rev)
+		if [ -d "${peakdir}/${name}_forward" ]; then
 
-		file_f=$(ls ${splitdir}/*.bam | grep -e ${name} | grep -v "input" | grep -e "forward")
-		control_f=$(ls ${splitdir}*.bam | grep -e ${controlname} | grep -e "input" | grep -v "RNase" | grep -e "forward")
+			echo "peaks already called"
 
-		file_r=$(ls ${splitdir}/*.bam | grep -e ${name} | grep -v "input" | grep -e "reverse")
-		control_r=$(ls ${splitdir}*.bam | grep -e ${controlname} | grep -e "input" | grep -v "RNase" | grep -e "reverse")
-		
-		
-		macs2 callpeak -t ${file_f} -c ${control_f} \
-		--name ${name}_forward --outdir ${peakdir}/${name}_forward \
-		--format BAMPE -g 1.2e8 --nomodel -q 0.01
-		
-		macs2 callpeak -t ${file_r} -c ${control_r} \
-		--name ${name}_reverse --outdir ${peakdir}/${name}_reverse \
-		--format BAMPE -g 1.2e8 --nomodel -q 0.01
-		
+		else
+
+			echo "no peak file found, proceeding to calling peaks for ${name}"
+
+			if echo ${name} | grep -e "RNH" || echo ${name} | grep -e "RNase"; then
+
+				controlname=$(echo ${name} | rev | cut -d "-" -f3- | rev)
+
+			else
+
+				controlname=$(echo ${name} | rev | cut -d "-" -f2- | rev)
+
+			fi
+
+
+			file_f=$(ls ${splitdir}/*.bam | grep -e ${name} | grep -v "input" | grep -e "forward")
+			control_f=$(ls ${splitdir}/*.bam | grep -e ${controlname} | grep -e "input" | grep -v "RNase" | grep -e "forward")
+
+			file_r=$(ls ${splitdir}/*.bam | grep -e ${name} | grep -v "input" | grep -e "reverse")
+			control_r=$(ls ${splitdir}/*.bam | grep -e ${controlname} | grep -e "input" | grep -v "RNase" | grep -e "reverse")
+			
+
+			macs2 callpeak -t ${file_f} -c ${control_f} \
+			--name ${name}_forward --outdir ${peakdir}/${name}_forward \
+			--format BAMPE -g 1.2e8 --nomodel -q 0.01
+			
+			macs2 callpeak -t ${file_r} -c ${control_r} \
+			--name ${name}_reverse --outdir ${peakdir}/${name}_reverse \
+			--format BAMPE -g 1.2e8 --nomodel -q 0.01
+			
+		fi
 done
